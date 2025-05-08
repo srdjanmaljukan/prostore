2bf98a18cb2ef21d0c0da24b183d1115ab0e0145
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-05-09 01:59:31 +0200

    Setup Prisma

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
 
-  return ( <>
-  <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
-  </> );
-}
- 
+  return (
+    <>
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+    </>
+  );
+};
+
 export default Homepage;
