01f356840b593e0997348577a05d469296520853
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-05-11 02:57:03 +0200

    ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-array-constructor": "off",
+    },
+  }),
+]
 
 export default eslintConfig;
