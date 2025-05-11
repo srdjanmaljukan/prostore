4ad2d7be7cb403ecda872f5030a4ea5de12f9b17
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-05-11 22:16:41 +0200

    Add sign in

--- lib/validators.ts
+++ lib/validators.ts
@@ -20,3 +20,20 @@ export const insertProductSchema = z.object({
     price: currency, 
 
 })
+
+// Schema for signing users in
+export const signInFormSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters")
+});
+
+// Schema for signing users up
+export const signUpFormSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters"),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"]
+});
