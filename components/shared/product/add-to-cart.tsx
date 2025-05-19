d74c4219e1e0fc4a613b287331ce9fa2a53c22d6
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-05-19 09:36:39 +0200

    Add to cart component

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { CartItem } from "@/types";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    // Handle success add to cart
+    toast.success(`${item.name} added to cart.`, {
+      action: { label: "Go to Cart", onClick: () => router.push("/cart") },
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus /> Add To Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
