4ffbf84933f96da227ae2fdd2874242a1bc2203a
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-05-21 11:32:13 +0200

    Session Cart Id cookie

--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
+import { cookies, headers } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -75,6 +77,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
 
         return token;
+    },
+    authorized({request, auth}: any) {
+      // Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders
+          }
+        })
+
+        // Set newly generated session cart id in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+        return response;
+      } else {
+        return true;
+      }
     }
   },
   pages: {
--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,9 +1,30 @@
 "use server";
+import { cookies } from "next/headers";
 import { CartItem } from "@/types";
+import { formatError } from "../utils";
 
 export async function addItemToCart(data: CartItem) {
-    return {
-        success: false,
+    try {
+        // Check for cart cookie
+        const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+
+        if (!sessionCartId) throw new Error("Cart session not found.");
+
+        // Testing
+        console.log({
+            "Session Cart Id": sessionCartId
+        });
+
+        return {
+        success: true,
         message: "Item added to cart."
     }
+    } catch (error) {
+        return {
+            success: false,
+            message: formatError(error)
+        }
+    }
+
+    
 }
