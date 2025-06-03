39de498c89c66b6a0ce15bb44e1ae897fe4c043e
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-06-03 10:41:03 +0200

    Place Order page

--- lib/validators.ts
+++ lib/validators.ts
@@ -74,3 +74,24 @@ export const shippingAddressSchema = z.object({
 export const paymentMethodSchema = z.object({
     type: z.string().min(1, "Payment method is required.")
 }).refine((data) => PAYMENT_METHODS.includes(data.type), {path: ["type"], message: "Invalid Payment Method."})
+
+// Schema for inserting order
+export const insertOrderSchema = z.object({
+    userId: z.string().min(1, "User is required."),
+    itemsPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    totalPrice: currency,
+    paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {message: "Invalid payment method."}),
+    shippingAddress: shippingAddressSchema
+})
+
+// Schema for inserting order item
+export const insertOrderItemSchema = z.object({
+    productId: z.string(),
+    slug: z.string(),
+    image: z.string(),
+    name: z.string(),
+    price: currency,
+    qty: z.number()
+})
