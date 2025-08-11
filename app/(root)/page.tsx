766c31bae688611972933c54f107ec752a0194b9
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-08-12 01:51:19 +0200

    Add deal countdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -12,6 +14,8 @@ const Homepage = async () => {
     {featuredProducts.length > 1 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
